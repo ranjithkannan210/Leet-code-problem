729717f30024a72bebdc3b8bedeb3080fad8af53
Ranjith k <ranjithkannan210@gmail.com>
2026-08-10 20:31:25 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (31.09%) - LeetSync

---
+++ 96-unique-binary-search-trees/unique-binary-search-trees.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numTrees(int n) {
+    long a = 1;
+    if(n==0||n==1){
+        return 1;
+    }
+    for (int i = 0; i < n; ++i) {
+        a = a * 2 * (2 * i + 1) / (i + 2);
+    }
+    return (int)a;
+}
+        
+    }
