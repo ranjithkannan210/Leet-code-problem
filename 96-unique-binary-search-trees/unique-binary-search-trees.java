2e6879902c64711833d31a09c7ebb3f3a5349fff
Ranjith k <ranjithkannan210@gmail.com>
2026-08-10 20:33:56 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (51.07%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/unique-binary-search-trees.java
@@ -5,7 +5,7 @@ class Solution {
         return 1;
     }
     for (int i = 0; i < n; ++i) {
-        a = a * 2 * (2 * i + 1) / (i + 2);
+        a = a * 2 * ((2 * i )+ 1) / (i + 2);
     }
     return (int)a;
 }
